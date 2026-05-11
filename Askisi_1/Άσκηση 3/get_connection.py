7a1d6d7cfda594a634fd5daecbb5f920a4b0564e
Panagiotis Christoforidis <panagiotis30x@gmail.com>
2026-05-11 16:52:47 +0300

    Deleted unnecessary directory

--- Askisi_1/Άσκηση 3/get_connection.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-def get_connection():
-    return pymysql.connect(
-        host="localhost",
-        user="root",
-        password="password",
-        database="flight",
-        cursorclass=pymysql.cursors.DictCursor
-    )
